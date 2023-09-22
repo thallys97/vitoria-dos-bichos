bb17bab52928273032dccf9e2a7dcacca85e5356
thallys roque <thallysbandeira97@gmail.com>
2023-09-22 11:50:52 -0500

    desenvolvendo a view de layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Associação Vitória dos Bichos | Vitória dos Bichos</title>
+    <!-- Inclua aqui seus links para folhas de estilo CSS, se necessário -->
+</head>
+<body>
+
+    {{ $slot }}
+    
+    <footer>
+        <p>Vitória dos Bichos@ 2022 - Todos os Direitos Reservados</p>        
+    </footer>
+</body>
+</html>
