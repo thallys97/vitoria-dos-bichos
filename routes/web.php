d17e671fd45a702d34c2c7f6ec4068cab63739c7
thallys roque <thallysbandeira97@gmail.com>
2023-09-22 11:54:39 -0500

    fazendo com que a rota para a home/ execute o método index

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [HomeControllerController::class, 'index']);
