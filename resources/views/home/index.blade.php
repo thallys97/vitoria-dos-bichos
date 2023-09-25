8d9c03555c837c89b877ce4a9d488efd72c0afcc
thallys roque <thallysbandeira97@gmail.com>
2023-09-25 14:43:55 -0500

    adicionei a classe bootstrap navbar-dark na navbar

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,9 +3,8 @@
    
     <header class="bg-dark d-flex flex-column justify-content-center align-items-center">
         <img src="{{ asset('images/logo-AVB.png') }}" class="w-50" alt="logo da associação vitória dos bichos">
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container-fluid">
-              {{-- <a class="navbar-brand" href="#">Navbar</a> --}}
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
