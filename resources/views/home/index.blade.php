0578db5bf26d23951dbeded7bb143c5b533d1ab6
thallys roque <thallysbandeira97@gmail.com>
2023-09-26 12:48:58 -0500

    mexendo nos itens da navbar

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,8 +3,8 @@
    
     <header class="bg-dark d-flex flex-column justify-content-center align-items-center">
         <img src="{{ asset('images/logo-AVB.png') }}" class="w-50" alt="logo da associação vitória dos bichos">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-dark w-100">
+            <div class="container-fluid justify-content-center">
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
@@ -23,7 +23,7 @@
                     <a class="nav-link" href="#">Doação</a>
                   </li>
                   <li class="nav-item border text-center">
-                    <a class="nav-link" href="#">Contatos</a>
+                    <a class="nav-link nav-link-color" href="#" style="color: blueviolet">Contatos</a>
                   </li>
                 </ul>
               </div>
