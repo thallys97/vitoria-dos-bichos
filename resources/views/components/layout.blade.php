4a7ba93d649ffb9652203a549e810f8b2b8d653e
thallys roque <thallysbandeira97@gmail.com>
2023-10-04 11:41:56 -0500

    adicionei um ícone de favicon à página inicial do site

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Associação Vitória dos Bichos | Vitória dos Bichos</title>
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/home/home.css') }}">
     <script src="https://kit.fontawesome.com/07138ba291.js" crossorigin="anonymous"></script>
