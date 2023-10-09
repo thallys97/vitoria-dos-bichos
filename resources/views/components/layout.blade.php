b3205c9169c18bd84fe6113bc818ab981d541346
thallys roque <thallysbandeira97@gmail.com>
2023-10-09 16:28:51 -0500

    adicionei o item Blog no nav do meu cabeçalho

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -41,6 +41,9 @@
                     <li class="nav-item cabecalho-navbar-item border border-secondary  text-center">
                       <a class="nav-link nav-link-custom fw-bold" href="https://avitoriadosbichos.com.br/#contatos-telefones">Contatos</a>
                     </li>
+                    <li class="nav-item cabecalho-navbar-item border border-secondary  text-center">
+                        <a class="nav-link nav-link-custom fw-bold" href="#">Blog</a>
+                    </li>
                   </ul>
                 </div>
               </div>
