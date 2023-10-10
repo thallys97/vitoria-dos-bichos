eb6121cf196a64ac42b908a35ff8da9dab2a5e67
thallys roque <thallysbandeira97@gmail.com>
2023-10-10 16:20:29 -0500

    atualizei a migration e a model de post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
     use HasFactory;
+
+    protected $fillable = ['title', 'content', 'author_id'];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'post_category');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'post_tag');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function media()
+    {
+        return $this->hasMany(Medium::class);
+    }
 }
+
+
--- database/migrations/2023_10_10_184237_create_posts_table.php
+++ database/migrations/2023_10_10_184237_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->unsignedBigInteger('author_id');
             $table->timestamps();
         });
     }
