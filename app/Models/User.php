17c62d884841c51a2392829bb635aadd8984c11e
thallys roque <thallysbandeira97@gmail.com>
2023-10-11 15:28:23 -0500

    atualizei a model de User

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -42,4 +43,22 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function isReader() 
+    { 
+        return $this->role === 'leitor'; 
+    } 
+    public function isAuthor() 
+    { 
+        return $this->role === 'autor'; 
+    } 
+    public function isEditor() 
+    { 
+        return $this->role === 'editor'; 
+    } 
+    public function isAdmin() 
+    { 
+        return $this->role === 'administrador'; 
+    }
 }
