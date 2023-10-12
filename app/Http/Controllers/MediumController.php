f65057165263d69c69ded31a738cf267a7ddb8ff
thallys roque <thallysbandeira97@gmail.com>
2023-10-12 15:04:12 -0500

    alterei alguns palavras para o inglês no MediumController

--- app/Http/Controllers/MediumController.php
+++ app/Http/Controllers/MediumController.php
@@ -11,23 +11,23 @@ class MediumController extends Controller
     public function create()
     {
         // Exibir o formulário de upload de mídia
-        return view('midias.create');
+        return view('media.create');
     }
 
     public function store(Request $request)
     {
         // Fazer upload de uma nova mídia
-        $midia = new Medium();
-        $midia->post_id = $request->input('post_id');
+        $medium = new Medium();
+        $medium->post_id = $request->input('post_id');
 
         // Manipular o arquivo de mídia
-        if ($request->hasFile('caminho')) {
-            $file = $request->file('caminho');
-            $path = $file->store('midias', 'public');
-            $midia->caminho = $path;
+        if ($request->hasFile('path')) {
+            $file = $request->file('path');
+            $path = $file->store('media', 'public');
+            $medium->path = $path;
         }
 
-        $midia->save();
+        $medium->save();
 
         return redirect()->back()->with('success', 'Mídia adicionada com sucesso.');
     }
@@ -35,8 +35,8 @@ class MediumController extends Controller
     public function destroy($id)
     {
         // Excluir uma mídia
-        $midia = Medium::findOrFail($id);
-        $midia->delete();
+        $medium = Medium::findOrFail($id);
+        $medium->delete();
 
         return redirect()->back()->with('success', 'Mídia excluída com sucesso.');
     }
