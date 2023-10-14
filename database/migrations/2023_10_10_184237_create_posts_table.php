4bc97bcdfa5f3648ee51790ed4a5f69799fc2eb3
thallys roque <thallysbandeira97@gmail.com>
2023-10-14 00:38:10 -0500

    atualizei as minhas migrations, trocando author por user

--- database/migrations/2023_10_10_184237_create_posts_table.php
+++ database/migrations/2023_10_10_184237_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('content');
-            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
     }
--- database/migrations/2023_10_10_185149_create_comments_table.php
+++ database/migrations/2023_10_10_185149_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id(); 
             $table->unsignedBigInteger('post_id'); 
-            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->string('email');
             $table->text('content'); 
