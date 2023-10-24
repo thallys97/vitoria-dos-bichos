165778acb1c53097dfacbeb3765653175762b6f1
thallys roque <thallysbandeira97@gmail.com>
2023-10-24 14:23:50 -0500

    implementei os métodos create e store no UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,6 +15,34 @@ class UserController extends Controller
         return view('users.index', compact('users'));
     }
 
+    public function create()
+    {
+        return view('users.create'); // Exibir o formulário de criação de usuário
+    }
+
+    public function store(Request $request)
+    {
+        // Validação dos campos
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'role' => 'required',
+            'phone' => 'required|regex:/^\([0-9]{2}\) [0-9]{4,5}-[0-9]{4}$/',
+        ]);
+
+        // Criação do usuário
+        $user = new User;
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password'));
+        $user->role = $request->input('role');
+        $user->phone = $request->input('phone'); 
+
+        $user->save();
+
+        return redirect('/users')->with('success', 'Usuário criado com sucesso.');
+    }
 
     public function edit($id)
     {
@@ -25,11 +53,11 @@ class UserController extends Controller
 
     public function update(Request $request, $id)
     {
-        // Atualizar informações de um usuário
-        $user = User::findOrFail($id);
+        $user = User::findOrFail($id); // Atualizar informações de um usuário
         $user->name = $request->input('name');
         $user->email = $request->input('email');
         $user->role = $request->input('role');
+        $user->phone = $request->input('phone'); // Adicione essa linha para atualizar o número de telefone
         $user->save();
 
         return redirect('/users')->with('success', 'Informações do usuário atualizadas com sucesso.');
