a903f1856c12b1378add906a90889bead27e5e0a
thallys roque <thallysbandeira97@gmail.com>
2023-10-26 12:41:30 -0500

    envolvi a view auth.login com a view minimal-layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,17 @@
-@extends('layouts.app') <!-- Suponha que você tem um layout padrão chamado 'app' -->
+<x-minimal-layout>
 
-@section('content')
-<div class="container">
+<main class="container">
     <div class="row justify-content-center">
         <div class="col-md-6">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">Login</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group">
-                            <label for="email">{{ __('E-Mail Address') }}</label>
+                            <label for="email">E-Mail</label>
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             @error('email')
                             <span class="invalid-feedback" role="alert">
@@ -22,7 +21,7 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="password">{{ __('Password') }}</label>
+                            <label for="password">Senha</label>
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                             @error('password')
                             <span class="invalid-feedback" role="alert">
@@ -36,14 +35,14 @@
                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                 <label class="form-check-label" for="remember">
-                                    {{ __('Remember Me') }}
+                                    Lembrar-me
                                 </label>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <button type="submit" class="btn btn-primary">
-                                {{ __('Login') }}
+                                Login
                             </button>
                         </div>
                     </form>
@@ -51,5 +50,6 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+</main>
+
+</x-minimal-layout>
