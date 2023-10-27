e0574ac3032b9332f5123d27d7f14e5d6210ed89
thallys roque <thallysbandeira97@gmail.com>
2023-10-27 17:12:48 -0500

    coloquei o conteúdo da view de dashboard em um card

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,20 +3,28 @@
     <main class="container vh-100 d-flex flex-column justify-content-center align-items-center ">
         <h1>Painel Administrativo</h1>
 
-        <div class="mb-3">
-            <a href="{{ route('posts.index') }}" class="btn btn-primary">Listagem de Posts</a>
+        <div class="card">
+            <div class="card-header">Opções</div>
+            <div class="card-body">
+                <div class="mb-3">
+                    <a href="{{ route('posts.index') }}" class="btn btn-primary">Listagem de Posts</a>
+                </div>
+        
+                <div class="mb-3">
+                    <a href="{{ route('users.index') }}" class="btn btn-primary">Listagem de Usuários</a>
+                </div>
+        
+                <div>
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <div class="mb-3">
-            <a href="{{ route('users.index') }}" class="btn btn-primary">Listagem de Usuários</a>
-        </div>
-
-        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-            Logout
-        </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
+        
 
     </main>
 
