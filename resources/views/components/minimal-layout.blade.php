5c059ddf73790980c264726eea36762c74427c88
thallys roque <thallysbandeira97@gmail.com>
2023-10-27 15:03:41 -0500

    criei arquivo login.css e importei para ser usada na minha view de login

--- resources/views/components/minimal-layout.blade.php
+++ resources/views/components/minimal-layout.blade.php
@@ -7,6 +7,7 @@
     <title>Associação Vitória dos Bichos | Vitória dos Bichos</title>
     <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
     <script src="https://kit.fontawesome.com/07138ba291.js" crossorigin="anonymous"></script>
 </head>
 <body>
