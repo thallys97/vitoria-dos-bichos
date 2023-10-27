898a6ceb47163bcdb15f5eac53abb00e1d933377
thallys roque <thallysbandeira97@gmail.com>
2023-10-27 16:00:08 -0500

    adicionei cores ao formulário de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,36 +1,36 @@
 <x-minimal-layout>
 
-<main class="container vh-100 d-flex justify-content-center align-items-center">
+<main class="container vh-100 d-flex justify-content-center align-items-center login-main">
     <div class="row justify-content-center w-100">
         <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">Login</div>
+            <div class="card custom-card">
+                <div class="card-header"><h3 class="fw-bold">Login</h3></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login.request') }}">
                         @csrf
 
-                        <div class="form-group mb-3">
+                        <div class="form-group mb-3 fw-bold">
                             <label for="email">E-Mail</label>
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             @error('email')
                             <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
+                                <strong class="fs-5">{{ $message }}</strong>
                             </span>
                             @enderror
                         </div>
 
-                        <div class="form-group mb-3">
+                        <div class="form-group mb-3 fw-bold">
                             <label for="password">Senha</label>
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                             @error('password')
                             <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
+                                <strong class="fs-5">{{ $message }}</strong>
                             </span>
                             @enderror
                         </div>
 
-                        <div class="form-group mb-3">
+                        <div class="form-group mb-3 fw-bold">
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -40,7 +40,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group fw-bold">
                             <button type="submit" class="btn btn-primary">
                                 Login
                             </button>
