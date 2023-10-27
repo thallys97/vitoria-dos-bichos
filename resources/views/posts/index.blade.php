ef512d89034b5c45cf7a6d98e1f7e40fdc99d4de
thallys roque <thallysbandeira97@gmail.com>
2023-10-27 11:15:29 -0500

    caso não tenha posts, recebo uma mensagem

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,33 +3,38 @@
     <main class="container">
         <h1>Lista de Posts</h1>
         <a href="{{ route('posts.create') }}" class="btn btn-success">Criar Novo Post</a>
-        <div class="row">
-            @foreach ($posts as $post)
-                <div class="col-12 col-md-6 col-lg-4 mb-4">
-                    <div class="card">
-                        @if ($post->media->count() > 0)
-                            <a href="{{ route('posts.show', $post->id) }}">
-                                <img src="{{ asset('storage/' . $post->media[0]->path) }}" class="card-img-top" alt="Imagem do post">
-                            </a>        
-                        @endif
-                        <div class="card-body">
-                            <a href="{{ route('posts.show', $post->id) }}">
-                                <h5 class="card-title">{{ $post->title }}</h5>
-                            </a>
-                            <a href="{{ route('posts.show', $post->id) }}">        
-                                <p class="card-text">{{ Str::limit($post->content, 100) }}</p>
-                            </a>    
-                            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary">Editar Post</a>
-                            <form id="delete-form-{{ $post->id }}" action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display: inline;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="button" class="btn btn-danger delete-post" data-post-id="{{ $post->id }}">Excluir Post</button>
-                            </form>
+
+        @if (count($posts) > 0)
+            <div class="row">
+                @foreach ($posts as $post)
+                    <div class="col-12 col-md-6 col-lg-4 mb-4">
+                        <div class="card">
+                            @if ($post->media->count() > 0)
+                                <a href="{{ route('posts.show', $post->id) }}">
+                                    <img src="{{ asset('storage/' . $post->media[0]->path) }}" class="card-img-top" alt="Imagem do post">
+                                </a>        
+                            @endif
+                            <div class="card-body">
+                                <a href="{{ route('posts.show', $post->id) }}">
+                                    <h5 class="card-title">{{ $post->title }}</h5>
+                                </a>
+                                <a href="{{ route('posts.show', $post->id) }}">        
+                                    <p class="card-text">{{ Str::limit($post->content, 100) }}</p>
+                                </a>    
+                                <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary">Editar Post</a>
+                                <form id="delete-form-{{ $post->id }}" action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display: inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="button" class="btn btn-danger delete-post" data-post-id="{{ $post->id }}">Excluir Post</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
-        </div>
+                @endforeach
+            </div>
+        @else
+            <p>Nenhum post foi criado ainda.</p>
+        @endif        
     </main>
 
   <script src="{{ asset('js/posts/delete-modal-confirmation.js') }}"></script> 
