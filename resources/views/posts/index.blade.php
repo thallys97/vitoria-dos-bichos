70844280540ff28dcbe64f26fafb7fefe957d96e
thallys roque <thallysbandeira97@gmail.com>
2023-10-31 12:44:39 -0500

    Adicionei estilos personalizados e ajustei layout em posts/posts.css e posts/index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,34 +1,40 @@
 <x-layout>
 
+    @push('head')
+        <link rel="stylesheet" href="{{ asset('css/posts/posts.css') }}">
+    @endpush   
+
     <main class="container">
-        <h1>Lista de Posts</h1>
-        @auth
-            <a href="{{ route('posts.create') }}" class="btn btn-success">Criar Novo Post</a>
-        @endauth
+        <div class="mb-4">
+            <h1 class="title">Lista de Posts</h1>
+            @auth
+                <a href="{{ route('posts.create') }}" class="btn button-post rounded-pill fw-bold fs-5 mt-4 mb-2">Criar Novo Post</a>
+            @endauth
+        </div>
 
         @if (count($posts) > 0)
             <div class="row">
                 @foreach ($posts as $post)
                     <div class="col-12 col-md-6 col-lg-4 mb-4">
-                        <div class="card">
+                        <div class="card custom-card">
                             @if ($post->media->count() > 0)
                                 <a href="{{ route('posts.show', $post->id) }}">
                                     <img src="{{ asset('storage/' . $post->media[0]->path) }}" class="card-img-top" alt="Imagem do post">
                                 </a>        
                             @endif
                             <div class="card-body">
-                                <a href="{{ route('posts.show', $post->id) }}">
-                                    <h5 class="card-title">{{ $post->title }}</h5>
+                                <a href="{{ route('posts.show', $post->id) }}" class="card-link text-center">
+                                    <h2 class="card-title fw-bold display-4 mt-3">{{ $post->title }}</h2>
                                 </a>
-                                <a href="{{ route('posts.show', $post->id) }}">        
-                                    <p class="card-text">{{ Str::limit($post->content, 100) }}</p>
+                                <a href="{{ route('posts.show', $post->id) }}" class="card-link">        
+                                    <p class="card-text fs-5 mb-4">{{ Str::limit($post->content, 100) }}</p>
                                 </a>
                                 @auth
-                                    <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary">Editar Post</a>
+                                    <a href="{{ route('posts.edit', $post->id) }}" class="btn button-post rounded-pill fw-bold">Editar Post</a>
                                     <form id="delete-form-{{ $post->id }}" action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display: inline;">
                                         @csrf
                                         @method('DELETE')
-                                        <button type="button" class="btn btn-danger delete-post" data-post-id="{{ $post->id }}">Excluir Post</button>
+                                        <button type="button" class="btn btn-danger rounded-pill delete-post" data-post-id="{{ $post->id }}">Excluir Post</button>
                                     </form>
                                 @endauth
                             </div>
