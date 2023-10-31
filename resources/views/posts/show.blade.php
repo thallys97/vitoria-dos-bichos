deb7de40331352c3d32483d591527b146d6f21b8
thallys roque <thallysbandeira97@gmail.com>
2023-10-31 15:08:51 -0500

    Adicionando estilos personalizados e ajustando layout em posts/posts.css e posts/show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,21 @@
 <x-layout>
 
-    <main class="container">
-        <h1>{{ $post->title }}</h1>
-        
-        @if ($post->media->count() > 0)
-            <img src="{{ asset('storage/' . $post->media[0]->path) }}" class="img-fluid" alt="Imagem do post">
-        @endif
+    @push('head')
+        <link rel="stylesheet" href="{{ asset('css/posts/posts.css') }}">
+    @endpush
 
-        <p>{{ $post->content }}</p>
-        
+    <main class="container">
+        <section class="post">
+            <h1 class="text-center fw-bold post-title">{{ $post->title }}</h1>
+            
+            @if ($post->media->count() > 0)                
+                <img src="{{ asset('storage/' . $post->media[0]->path) }}" class="img-fluid" alt="Imagem do post">                
+            @endif
+
+            <p class="post-content">{{ $post->content }}</p>
+
+        </section>    
+            
         <a href="{{ route('posts.index') }}" class="btn btn-secondary">Lista de Posts</a>
         @auth
             <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary">Editar Post</a>
@@ -17,7 +24,8 @@
                 @method('DELETE')
                 <button type="button" class="btn btn-danger delete-post" data-post-id="{{ $post->id }}">Excluir Post</button>
             </form>
-        @endauth    
+        @endauth
+            
     </main>
 
     <script src="{{ asset('js/posts/delete-modal-confirmation.js') }}"></script>
