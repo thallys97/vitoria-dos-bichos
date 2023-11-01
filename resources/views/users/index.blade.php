10dcfbb9d34e0c7a8f7c4ba7ce5eb8061b1166d0
thallys roque <thallysbandeira97@gmail.com>
2023-11-01 15:05:28 -0500

    Atualizei a visualização da lista de usuários com um novo título e melhorias no estilo das ações do usuário.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,41 +5,43 @@
 @endpush 
 
     <main class="container">
-        <h2>Lista de Usuários</h2>
-        <a href="{{ route('users.create') }}" class="btn btn-primary">Criar Usuário</a>
+        <h1 class="title">Lista de Usuários</h1>
+        <a href="{{ route('users.create') }}" class="btn button-user rounded-pill fw-bold my-3">Criar Usuário</a>
 
         @if (count($users) > 0)
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Email</th>
-                        <th>Telefone</th>
-                        <th>Função</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($users as $user)
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
                         <tr>
-                            <td>{{ $user->id }}</td>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-                            <td>{{ $user->phone }}</td>
-                            <td>{{ $user->role }}</td>
-                            <td>
-                                <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary">Editar</a>
-                                <form id="delete-form-{{ $user->id }}"  action="{{ route('users.destroy', $user->id) }}" method="POST" style="display: inline;">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="button" class="btn btn-danger delete-user" data-user-id="{{ $user->id }}">Excluir</button>
-                                </form>
-                            </td>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Email</th>
+                            <th>Telefone</th>
+                            <th>Função</th>
+                            <th>Ações</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->phone }}</td>
+                                <td>{{ $user->role }}</td>
+                                <td>
+                                    <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary">Editar</a>
+                                    <form id="delete-form-{{ $user->id }}"  action="{{ route('users.destroy', $user->id) }}" method="POST" style="display: inline;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="button" class="btn btn-danger delete-user" data-user-id="{{ $user->id }}">Excluir</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>    
         @else
             <p>Nenhum usuário encontrado.</p>
         @endif
