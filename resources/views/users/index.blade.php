9d2e355f2b3578ba84f1ea1c9b80592054339869
thallys roque <thallysbandeira97@gmail.com>
2023-11-02 11:47:57 -0500

    Adicionei estilo para a mensagem 'Nenhum usuário foi criado ainda' no caso em que nenhum usuário seja encontrado na página.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,39 +9,41 @@
         <a href="{{ route('users.create') }}" class="btn button-user rounded-pill fw-bold my-3">Criar Usuário</a>
 
         @if (count($users) > 0)
-            <div class="table-responsive">
-                <table class="table table-striped table-bordered">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nome</th>
-                            <th>Email</th>
-                            <th>Telefone</th>
-                            <th>Função</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($users as $user)
+            <section class="users">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered">
+                        <thead>
                             <tr>
-                                <td class="align-middle">{{ $user->id }}</td>
-                                <td class="align-middle">{{ $user->name }}</td>
-                                <td class="align-middle">{{ $user->email }}</td>
-                                <td class="align-middle">{{ $user->phone }}</td>
-                                <td class="align-middle">{{ $user->role }}</td>
-                                <td class="align-middle">
-                                    <a href="{{ route('users.edit', $user->id) }}" class="btn button-user rounded-pill fw-bold button-user-edit">Editar</a>
-                                    <form id="delete-form-{{ $user->id }}"  action="{{ route('users.destroy', $user->id) }}" method="POST" style="display: inline;">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="button" class="btn btn-danger rounded-pill delete-user button-delete fw-bold" data-user-id="{{ $user->id }}">Excluir</button>
-                                    </form>
-                                </td>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Email</th>
+                                <th>Telefone</th>
+                                <th>Função</th>
+                                <th>Ações</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>    
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td class="align-middle">{{ $user->id }}</td>
+                                    <td class="align-middle">{{ $user->name }}</td>
+                                    <td class="align-middle">{{ $user->email }}</td>
+                                    <td class="align-middle">{{ $user->phone }}</td>
+                                    <td class="align-middle">{{ $user->role }}</td>
+                                    <td class="align-middle">
+                                        <a href="{{ route('users.edit', $user->id) }}" class="btn button-user rounded-pill fw-bold button-user-edit">Editar</a>
+                                        <form id="delete-form-{{ $user->id }}"  action="{{ route('users.destroy', $user->id) }}" method="POST" style="display: inline;">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="button" class="btn btn-danger rounded-pill delete-user button-delete fw-bold" data-user-id="{{ $user->id }}">Excluir</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </section>    
         @else
             <section class="no-users">
                 <div class="no-users-card text-center">
