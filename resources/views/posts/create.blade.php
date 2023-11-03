00d0c60315eb8c93d0250833842b2e0834a3743a
thallys roque <thallysbandeira97@gmail.com>
2023-11-03 15:24:20 -0500

    Fiz melhorias no layout do formulário de criação de post: centralizado o formulário, ajustadas as larguras dos campos em diferentes tamanhos de tela e estilizados os elementos de entrada de arquivo.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -19,32 +19,34 @@
 
     <form method="post" action="{{ route('posts.store') }}" enctype="multipart/form-data">
         @csrf
+        
+        <div class="container-lg form-custom">
+            <div class="form-group">
+                <label for="title" class="form-label fs-5">Título do Post:</label>
+                <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+            </div>
 
-        <div class="form-group">
-            <label for="title" class="form-label fs-5">Título do Post:</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
-        </div>
+            <div class="form-group">
+                <label for="content" class="form-label fs-5">Conteúdo do Post:</label>
+                <textarea class="form-control" id="content" name="content">{{ old('content') }}</textarea>
+            </div>
 
-        <div class="form-group">
-            <label for="content" class="form-label fs-5">Conteúdo do Post:</label>
-            <textarea class="form-control" id="content" name="content">{{ old('content') }}</textarea>
-        </div>
+            <div class="form-group">
+                <label for="media" class="form-label fs-5">Envie a imagem para o post:</label>
+                <input type="file" id="media" class="form-control form-control-file" name="media" accept="image/*">
+            </div>
 
-        <div class="form-group">
-            <label for="media" class="form-label fs-5">Envie a imagem para o post:</label>
-            <input type="file" id="media" class="form-control w-100" name="media" accept="image/*">
-        </div>
+            <span id="file-icon" class="fa-stack fa-2x" style="cursor: pointer; display: none;">
+                <i class="fas fa-circle fa-stack-2x"></i>
+                <i class="fas fa-file-upload fa-stack-1x fa-inverse"></i>
+            </span>
 
-        <span id="file-icon" class="fa-stack fa-2x" style="cursor: pointer; display: none;">
-            <i class="fas fa-circle fa-stack-2x"></i>
-            <i class="fas fa-file-upload fa-stack-1x fa-inverse"></i>
-        </span>
+            <span id="file-name" style="display: none;"></span>
 
-        <span id="file-name" style="display: none;"></span>
-
-        <div class="form-buttons d-flex justify-content-evenly">
-            <button type="submit" class="btn btn-primary button-post rounded-pill fw-bold">Criar Post</button>
-            <a href="{{ route('posts.index') }}" class="btn btn-secondary button-post-list rounded-pill fw-bold">Lista de Posts</a>
+            <div class="form-buttons d-flex justify-content-evenly">
+                <button type="submit" class="btn btn-primary button-post rounded-pill fw-bold">Criar Post</button>
+                <a href="{{ route('posts.index') }}" class="btn btn-secondary button-post-list rounded-pill fw-bold">Lista de Posts</a>
+            </div>
         </div>
 
     </form>
