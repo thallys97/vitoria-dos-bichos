71153a193c456ddaf303dd419b1978e758da4f0d
thallys roque <thallysbandeira97@gmail.com>
2023-11-03 16:10:04 -0500

    Fiz melhorias no layout do formulário de edição de post: ajustadas as larguras dos campos em diferentes tamanhos de tela, estilizado o elemento de entrada de arquivo e adicionado estilo à imagem atual.

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -22,41 +22,43 @@
             @csrf
             @method('PUT')
 
-            <div class="form-group">
-                <label for="title" class="form-label fs-5">Título:</label>
-                <input type="text" name="title" class="form-control" value="{{ $post->title }}">
-            </div>
-            
+            <div class="container-lg form-custom">
 
-            @if ($post->media->count() > 0)
-                <div>    
-                    <h5 class="image-title fs-5 mb-2">Imagem atual:</h5>
-                    <img src="{{ asset('storage/' . $post->media[0]->path) }}" alt="Current Image" style="max-width: 100%">
-                </div>    
-            @endif
+                <div class="form-group">
+                    <label for="title" class="form-label fs-5">Título:</label>
+                    <input type="text" name="title" class="form-control" value="{{ $post->title }}">
+                </div>
+                
 
-            <div class="form-group">
-                <label for="media" class="form-label fs-5">(Opcional) Troque a imagem atual por outra:</label>
-                <input type="file" name="media"  id="media"  accept="image/*" class="form-control">
-            </div>
+                @if ($post->media->count() > 0)
+                    <div>    
+                        <h5 class="image-title fs-5 mb-2">Imagem atual:</h5>
+                        <img src="{{ asset('storage/' . $post->media[0]->path) }}" alt="Current Image" class="current-image img-fluid">
+                    </div>    
+                @endif
 
-            <span id="file-icon" class="fa-stack fa-2x" style="cursor: pointer; display: none;">
-                <i class="fas fa-circle fa-stack-2x"></i>
-                <i class="fas fa-file-upload fa-stack-1x fa-inverse"></i>
-            </span>
-    
-            <span id="file-name" style="display: none;"></span>
+                <div class="form-group">
+                    <label for="media" class="form-label fs-5">(Opcional) Troque a imagem atual por outra:</label>
+                    <input type="file" name="media"  id="media"  accept="image/*" class="form-control form-control-file">
+                </div>
 
-            <div class="form-group">
-                <label for="content" class="form-label fs-5">Conteúdo:</label>
-                <textarea name="content" class="form-control" rows="5">{{ $post->content }}</textarea>
-            </div>
+                <span id="file-icon" class="fa-stack fa-2x" style="cursor: pointer; display: none;">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fas fa-file-upload fa-stack-1x fa-inverse"></i>
+                </span>
+        
+                <span id="file-name" style="display: none;"></span>
 
-            <div class="form-buttons d-flex justify-content-evenly">
-                <button type="submit" class="btn button-post rounded-pill fw-bold">Atualizar Post</button>
-                <a href="{{ route('posts.index') }}" class="btn btn-secondary button-post-list rounded-pill fw-bold">Lista de Posts</a>
-            </div>
+                <div class="form-group">
+                    <label for="content" class="form-label fs-5">Conteúdo:</label>
+                    <textarea name="content" class="form-control" rows="5">{{ $post->content }}</textarea>
+                </div>
 
+                <div class="form-buttons d-flex justify-content-evenly">
+                    <button type="submit" class="btn button-post rounded-pill fw-bold">Atualizar Post</button>
+                    <a href="{{ route('posts.index') }}" class="btn btn-secondary button-post-list rounded-pill fw-bold">Lista de Posts</a>
+                </div>
+            </div>    
         </form>
     </main>
 
