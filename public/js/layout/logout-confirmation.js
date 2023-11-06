a4ea3b5956b9babecf646bdb280ced00540f5f1e
thallys roque <thallysbandeira97@gmail.com>
2023-11-06 17:02:18 -0500

    criei código js para abrir modal de confirmação de logout

--- public/js/layout/logout-confirmation.js
+++ public/js/layout/logout-confirmation.js
@@ -1,12 +1,34 @@
 // logout-confirmation.js
 
-// Quando o documento estiver pronto
-$(document).ready(function () {
-    // Capturando o clique na âncora
-    $(".confirm-logout").click(function (e) {
-        e.preventDefault(); // Prevenir o comportamento padrão do link
+document.addEventListener('DOMContentLoaded', function () {
+    // Capturando o clique na âncora com a classe "confirm-logout"
+    var logoutButtons = document.querySelectorAll('.confirm-logout');
 
-        // Exibindo o modal de confirmação
-        $("#logoutModal").modal("show");
+    logoutButtons.forEach(function (button) {
+        button.addEventListener('click', function (e) {
+            e.preventDefault();
+            // Exibindo o modal de confirmação
+            openLogoutConfirmationModal();
+        });
     });
+
+    // Capturando o clique no botão "Cancelar" do modal
+    var cancelButton = document.getElementById('cancelButton');
+
+    if (cancelButton) {
+        cancelButton.addEventListener('click', function () {
+            // Fechando o modal
+            closeLogoutConfirmationModal();
+        });
+    }
+
+    // Capturando o clique no botão "Sair" do modal
+    var confirmButton = document.getElementById('confirmButton');
+
+    if (confirmButton) {
+        confirmButton.addEventListener('click', function () {
+            // Submetendo o formulário de logout
+            document.getElementById('logout-form-layout').submit();
+        });
+    }
 });
