4b789f90f8ccaf0700b7b392450fa9c60b44f3a1
thallys roque <thallysbandeira97@gmail.com>
2023-11-09 11:20:30 -0500

    Atualizei os caminhos do favicon no layout.blade.php e modificei o public/favicon.ico.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Associação Vitória dos Bichos | Vitória dos Bichos</title>
-    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/components/layout.css') }}">
     <link rel="stylesheet" href="{{ asset('css/home/home.css') }}">
