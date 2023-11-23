ccd78a4e572b9fe22f8f33d44fc5e74d1509e7c2
thallys roque <thallysbandeira97@gmail.com>
2023-11-23 12:16:42 -0500

    implementei as funções do VideoController

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Video;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VideoController extends Controller
+{
+    public function index()
+    {
+        $videos = Video::all();
+        return view('videos.index', compact('videos'));
+    }
+
+    public function create()
+    {
+        // Adicione lógica aqui, se necessário
+        return view('videos.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'path' => 'required|string',
+            'title' => 'nullable|string',
+            'description' => 'nullable|string',
+        ]);
+
+        Video::create($request->all());
+
+        return redirect()->route('videos.index')->with('success', 'Vídeo criado com sucesso!');
+    }
+
+    public function show(Video $video)
+    {
+        return view('videos.show', compact('video'));
+    }
+
+    public function edit(Video $video)
+    {
+        // Adicione lógica aqui, se necessário
+        return view('videos.edit', compact('video'));
+    }
+
+    public function update(Request $request, Video $video)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'path' => 'required|string',
+            'title' => 'nullable|string',
+            'description' => 'nullable|string',
+        ]);
+
+        $video->update($request->all());
+
+        return redirect()->route('videos.index')->with('success', 'Vídeo atualizado com sucesso!');
+    }
+
+    public function destroy(Video $video)
+    {
+        $video->delete();
+
+        return redirect()->route('videos.index')->with('success', 'Vídeo removido com sucesso!');
+    }
+}
