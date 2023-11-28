3f26534b27937d5ccbaf3d5a0f07929c9551cef9
thallys roque <thallysbandeira97@gmail.com>
2023-11-28 14:50:24 -0500

    fiz alterações na estrutura do videos.index

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -24,34 +24,49 @@
             @auth
                 <a href="{{ route('videos.create') }}" class="btn button-post rounded-pill fw-bold fs-5 mt-4 mb-2">Registrar Novo Vídeo</a>
             @endauth
-        </div>  <!-- parei aqui -->
+        </div> 
     
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+   <!-- <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3"> -->
+        
             @forelse ($videos as $video)
-                <div class="col">
-                    <div class="card videos-card">
-                        <!-- Adicione seu código para exibir o vídeo, como um iframe do YouTube, por exemplo -->
-                        <!-- <iframe width="560" height="315" src="{{ $video->path }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
-                        <!-- Certifique-se de ajustar conforme o tipo de vídeo ou o método de exibição -->
-                        <img src="{{ asset('storage/' . $video->path) }}" class="card-img-top" alt="Imagem do vídeo">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $video->title }}</h5>
-                            <p class="card-text">{{ $video->description }}</p>
-                            <a href="{{ route('videos.edit', $video->id) }}" class="btn btn-primary">Editar</a>
-                            <form id="delete-form-{{ $video->id }}" action="{{ route('videos.destroy', $video->id) }}" method="POST" style="display: inline;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="button" class="btn btn-danger delete-video" data-video-id="{{ $video->id }}">Excluir</button>
-                            </form>
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-4 mb-4">
+                        <div class="card custom-card">
+                            
+                            <iframe width="560" height="315" src="{{ $video->path }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            
+                            <div class="card-body">
+                                <h2 class="card-title fw-bold mt-3">{{ $video->title }}</h5>
+                                <p class="card-text mb-4">{{ $video->description }}</p>
+
+                                @auth 
+                                    <div class="d-flex justify-content-evenly">       
+                                        <a href="{{ route('videos.edit', $video->id) }}" class="btn button-post rounded-pill fw-bold">Editar</a>
+                                        <form id="delete-form-{{ $video->id }}" action="{{ route('videos.destroy', $video->id) }}" method="POST" style="display: inline;">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="button" class="btn btn-danger rounded-pill delete-post button-delete fw-bold" data-video-id="{{ $video->id }}">Excluir</button>
+                                        </form>
+                                    </div>    
+                                @endauth    
+
+                            </div>
                         </div>
                     </div>
                 </div>
-            @empty
-                <div class="col text-center">
-                    <p class="no-videos-text fw-bold">Nenhum vídeo foi criado ainda.</p>
+
+                <div class="pagination d-flex justify-content-center mt-3">
+                    {{ $videos->links('custom-pagination') }}
                 </div>
-            @endforelse
-        </div>
+
+            @empty
+                <section class="no-posts">
+                    <div class="no-posts-card text-center">
+                        <p class="no-posts-text fw-bold">Nenhum post foi criado ainda.</p>
+                    </div>
+                </section> 
+            @endforelse   
+   <!-- </div> -->
     </main>
     
     <script src="{{ asset('js/videos/delete-modal-confirmation.js') }}"></script>
