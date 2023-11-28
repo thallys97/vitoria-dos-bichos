fdb18d4fd43a675b95fab2a50a343740877b64a5
thallys roque <thallysbandeira97@gmail.com>
2023-11-28 11:39:31 -0500

    Refatorei estilos no videos.css e ajustei o layout na create.blade.php para melhorar a aparência e usabilidade do formulário de adição de vídeos.

--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -4,8 +4,8 @@
         <link rel="stylesheet" href="{{ asset('css/videos/videos.css') }}">
     @endpush   
 
-    <main class="container py-5">
-        <h1 class="title d-none d-lg-block">Adicionar Novo Vídeo</h1>
+    <main class="container">
+        <h1 class="title">Adicionar Novo Vídeo</h1>
         
     @if ($errors->any())
         <div class="alert alert-danger">
@@ -17,25 +17,30 @@
         </div>
     @endif
 
-        <form action="{{ route('videos.store') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('videos.store') }}" method="POST">
             @csrf
 
-            <div class="mb-3">
-                <label for="title" class="form-label">Título do Vídeo (opcional)</label>
-                <input type="text" class="form-control" id="title" name="title">
-            </div>
+            <div class="container-lg form-custom">
+                <div class="form-group">
+                    <label for="title" class="form-label fs-5">Título do Vídeo (opcional)</label>
+                    <input type="text" class="form-control" id="title" name="title">
+                </div>
 
-            <div class="mb-3">
-                <label for="path" class="form-label">Link do Vídeo (YouTube, Vimeo, etc.)</label>
-                <input type="text" class="form-control" id="path" name="path" required>
-            </div>
+                <div class="form-group">
+                    <label for="path" class="form-label fs-5">Link do Vídeo (YouTube, Vimeo, etc.)</label>
+                    <input type="text" class="form-control" id="path" name="path" required>
+                </div>
 
-            <div class="mb-3">
-                <label for="description" class="form-label">Descrição do Vídeo (opcional)</label>
-                <textarea class="form-control" id="description" name="description" rows="4"></textarea>
-            </div>
+                <div class="form-group">
+                    <label for="description" class="form-label fs-5">Descrição do Vídeo (opcional)</label>
+                    <textarea class="form-control" id="description" name="description" rows="4"></textarea>
+                </div>
 
-            <button type="submit" class="btn btn-primary">Salvar Vídeo</button>
+                <div class="form-buttons d-flex justify-content-evenly">
+                    <button type="submit" class="btn btn-primary button-video rounded-pill fw-bold">Adicionar Vídeo</button>
+                    <a href="{{ route('videos.index') }}" class="btn btn-secondary button-video-list rounded-pill fw-bold">Lista de Vídeos</a>
+                </div>
+            </div>            
         </form>
     </main>
 
